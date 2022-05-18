ac590d8014d931bdbd3eae3c57d82129a6b2971b
Athena9355 <gte782a@gmail.com>
2022-05-18 12:15:31 -0700

    dictionary set up

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ from flask import Flask, render_template, request
 from __init__ import app
 import requests
 from cruddy.app_crud import app_crud
-from cruddy.app_crud_api import app_crud_api
 
 
 from pathlib import Path
 
 app.register_blueprint(app_crud)
-app.register_blueprint(app_crud_api)
-
 
 
 # connects default URL to render index.html
@@ -48,10 +45,6 @@ def search():
 def discussion():
     return render_template("discussion.html")
 
-@app.route('/drawing/')
-def drawing():
-    return render_template("drawing.html")
-
 @app.route('/kamya/',methods=['GET', 'POST'])
 def kamya():
     url = "https://random-words5.p.rapidapi.com/getMultipleRandom"
@@ -72,41 +65,16 @@ def kamya():
 def stub():
     return render_template("stub.html") #code for the function
 
-@app.route('/calendar/')
-def calendar():
-    return render_template("calendar.html")
 
-@app.route('/thread/')
-def thread():
-    return render_template("thread.html")
-
-@app.route('/plots/')
-def plots():
-    return render_template("plots.html")
-
-@app.route('/library/')
-def library():
-    return render_template("library.html")
-
-@app.route('/bookapi/',methods=['GET', 'POST'])
-def bookapi():
-    url = "https://google-books.p.rapidapi.com/volumes"
-
-    querystring = {"key":"undefined"}
-
-    headers = {
-        "X-RapidAPI-Host": "google-books.p.rapidapi.com",
-        "X-RapidAPI-Key": "baf45032bdmsh1fb0709d81a2d0ep1d1bfejsnb09953cc9b71"
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    return render_template("bookapi.html", book=response.json())
-    print(response.text)
+@app.route('/dictionary')
+def dictionary():
+    return render_template("dictionary.html")
 
 
 
 # runs the application on the development server
 if __name__ == "__main__":
-    app.run(debug=True,port=9000) #says "run this directly" app.run will run the server
+    app.run(debug=True,port=8080) #says "run this directly" app.run will run the server
 
 #index.html is standard
 
