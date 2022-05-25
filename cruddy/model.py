ee86ad657e2aab10bd0be9fc382b301b022016ca
kamyamahendru <kamya04mahendru@gmail.com>
2022-05-25 13:04:41 -0700

    login stuff

--- cruddy/model.py
+++ cruddy/model.py
@@ -1,9 +1,50 @@
 """ database dependencies to support Users db examples """
+
 from __init__ import db
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+# Define the 'Users Notes' table  with a relationship to Users within the model
+class Notes(db.Model):
+    __tablename__ = 'notes'
+
+    # Define the Notes schema
+    id = db.Column(db.Integer, primary_key=True)
+    note = db.Column(db.Text, unique=False, nullable=False)
+    # Define a relationship in Notes Schema to userID who originates the note, many-to-one (many notes to one user)
+    userID = db.Column(db.Integer, db.ForeignKey('users.userID'))
+
+    # Constructor of a Notes object, initializes of instance variables within object
+    def __init__(self, note, userID):
+        self.note = note
+        self.userID = userID
+
+    # Returns a string representation of the Notes object, similar to java toString()
+    # returns string
+    def __repr__(self):
+        return "Notes(" + str(self.id) + "," + self.note + "," + str(self.userID) + ")"
+
+    # CRUD create, adds a new record to the Notes table
+    # returns the object added or None in case of an error
+    def create(self):
+        try:
+            # creates a Notes object from Notes(db.Model) class, passes initializers
+            db.session.add(self)  # add prepares to persist person object to Notes table
+            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
+
+    # CRUD read, returns dictionary representation of Notes object
+    # returns dictionary
+    def read(self):
+        return {
+            "id": self.id,
+            "note": self.note,
+            "userID": self.userID
+        }
 
 # Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
 
