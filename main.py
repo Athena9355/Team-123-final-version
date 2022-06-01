42015e98211b275e34c9e3371601cfa914874540
nataliebeckwith <natalieb60251@stu.powayusd.com>
2022-05-31 19:24:55 -0700

    fixed merge conflict 2

--- main.py
+++ main.py
@@ -196,27 +196,6 @@ def bookapi():
     return render_template("bookapi.html", book=response.json())
     print(response.text)
 
-#@app.route('/dictionary/', methods=['GET','POST'])
-#def dictionary():
- #   try:
- #       keyword = request.form['keyword']
- #   except:
- #       keyword = "Book"
- #   url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/"
- #   querystring = {"entry":keyword}
- #   headers = {
-  #      'x-rapidapi-host': "twinword-word-graph-dictionary.p.rapidapi.com",
-   #     'x-rapidapi-key': "3d43659d98msh26d5e705bc7d8b6p1d6431jsnba44357aaf20"
- #   }
- #   response = requests.request("GET", url, headers=headers, params=querystring)
- #   if response.status_code<400:
- #       results = json.loads(response.content.decode("utf-8"))
- #       return render_template("dictionary.html", results=results, word=keyword)
- #   else:
- #       return render_template("dictionary.html", word=keyword)
-
-
-
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True,port=8001)
