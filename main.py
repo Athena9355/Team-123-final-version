fd355eb963e1f0164eeb860969c7d0c68e3e66db
nataliebeckwith <natalieb60251@stu.powayusd.com>
2022-05-31 19:22:26 -0700

    fixed merge conflict

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from flask_login import login_required
 import requests
 import json
 
+import json
+
 from cruddy.app_crud import app_crud
 
 from pathlib import Path
@@ -120,6 +122,26 @@ def discussion():
 def drawing():
     return render_template("drawing.html")
 
+@app.route('/dictionary/', methods=['GET','POST'])
+def dictionary():
+    try:
+        keyword = request.form['keyword']
+    except:
+        keyword = "study"
+    url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/"
+    querystring = {"entry":keyword}
+    headers = {
+        'x-rapidapi-host': "twinword-word-graph-dictionary.p.rapidapi.com",
+        'x-rapidapi-key': "3d43659d98msh26d5e705bc7d8b6p1d6431jsnba44357aaf20"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    if response.status_code<400:
+        results = json.loads(response.content.decode("utf-8"))
+        return render_template("dictionary.html", results=results, word=keyword)
+    else:
+        return render_template("dictionary.html", word=keyword)
+    # print(response.text)
+
 @app.route('/kamya/',methods=['GET', 'POST'])
 def kamya():
     url = "https://random-words5.p.rapidapi.com/getMultipleRandom"
@@ -174,29 +196,32 @@ def bookapi():
     return render_template("bookapi.html", book=response.json())
     print(response.text)
 
-@app.route('/dictionary/', methods=['GET','POST'])
-def dictionary():
-    try:
-        keyword = request.form['keyword']
-    except:
-        keyword = "Book"
-    url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/"
-    querystring = {"entry":keyword}
-    headers = {
-        'x-rapidapi-host': "twinword-word-graph-dictionary.p.rapidapi.com",
-        'x-rapidapi-key': "3d43659d98msh26d5e705bc7d8b6p1d6431jsnba44357aaf20"
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    if response.status_code<400:
-        results = json.loads(response.content.decode("utf-8"))
-        return render_template("dictionary.html", results=results, word=keyword)
-    else:
-        return render_template("dictionary.html", word=keyword)
+#@app.route('/dictionary/', methods=['GET','POST'])
+#def dictionary():
+ #   try:
+ #       keyword = request.form['keyword']
+ #   except:
+ #       keyword = "Book"
+ #   url = "https://twinword-word-graph-dictionary.p.rapidapi.com/definition/"
+ #   querystring = {"entry":keyword}
+ #   headers = {
+  #      'x-rapidapi-host': "twinword-word-graph-dictionary.p.rapidapi.com",
+   #     'x-rapidapi-key': "3d43659d98msh26d5e705bc7d8b6p1d6431jsnba44357aaf20"
+ #   }
+ #   response = requests.request("GET", url, headers=headers, params=querystring)
+ #   if response.status_code<400:
+ #       results = json.loads(response.content.decode("utf-8"))
+ #       return render_template("dictionary.html", results=results, word=keyword)
+ #   else:
+ #       return render_template("dictionary.html", word=keyword)
+
 
 
 # runs the application on the development server
 if __name__ == "__main__":
-    app.run(debug=True,port=8000) #says "run this directly" app.run will run the server
+    app.run(debug=True,port=8001)
+
+#says "run this directly" app.run will run the server
 
 #index.html is standard
 
